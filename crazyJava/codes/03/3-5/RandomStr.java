a3c1d687a04492194e1035e5b78f08225c141ae6
bug123luo <luo_3050120123@163.com>
2018-10-22 17:51:01 +0800

    20181022wujing

---
+++ crazyJava/codes/03/3-5/RandomStr.java
@@ -0,0 +1,17 @@
+public class RandomStr
+{
+	public static void main(String[] args){
+		//定义一个空字符串
+		String result = "";
+		//进行6次循环
+		for(int i=0; i<6;i++){
+			//生成一个97-122的int 型的整数
+			int intVal =(int)(Math.random()*26+97);
+			//将intVal强制转换为char后连接到result后面
+			result=result +(char)intVal;
+		}
+		
+		//输出随机字符串
+		System.out.println(result);
+	}
+}
