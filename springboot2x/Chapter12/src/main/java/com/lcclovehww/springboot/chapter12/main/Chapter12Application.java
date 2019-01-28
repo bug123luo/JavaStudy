06d6ad1ae5c43a3c8f6e76316c313b77dca6dfbc
bug123luo <luo_3050120123@163.com>
2019-01-28 16:38:13 +0800

    深入浅出springbootx 12-4实例提交

--- springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
+++ springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
@@ -2,7 +2,10 @@ package com.lcclovehww.springboot.chapter12.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication(scanBasePackages="com.lcclovehww.springboot.chapter12")
 /*@MapperScan(basePackages="com.lcclovehww.springboot.chapter12", annotationClass = Mapper.class)
@@ -29,24 +32,31 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 		SpringApplication.run(Chapter12Application.class, args);
 	}
 
-/*	@Override
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+		//密码编码器
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		
+		//可通过passwordEncoder.encode("abc")的到加密后的密码
 		String password1 = passwordEncoder.encode("abc");
 		System.out.println("abc="+password1);
+		//可通过passwordEncoder.encode("123456")的到加密后的密码
 		String password2 = passwordEncoder.encode("123456");
 		System.out.println("123456="+password2);
+		//使用内存存储
 		auth.inMemoryAuthentication()
+			//设置密码编码器
 			.passwordEncoder(passwordEncoder)
+			//注册用户admin,密码为abc,并赋予USER和ADMIN角色权限
 			.withUser("admin")
 			.password(password1)
 			.roles("USER","ADMIN")
+			//连接方法
 			.and()
+			//注册用户myuser,密码为123456，并赋予USER的角色权限
 			.withUser("myuser")
 			.password(password2)
 			.roles("USER");
-	}*/
+	}
 	
 	//和上一个函数实现的方法是相同的
 /*	@Override
