1b483febe346a3378c878671255d30050400c67e
bug123luo <luo_3050120123@163.com>
2019-01-28 16:23:19 +0800

    深入浅出springbootx 12-1 实例提交

--- springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
+++ springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
@@ -1,43 +1,29 @@
 package com.lcclovehww.springboot.chapter12.main;
 
-import javax.crypto.SecretKey;
-import javax.sql.DataSource;
-import org.apache.ibatis.annotations.Mapper;
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 
 @SpringBootApplication(scanBasePackages="com.lcclovehww.springboot.chapter12")
-@MapperScan(basePackages="com.lcclovehww.springboot.chapter12", annotationClass = Mapper.class)
-@EnableCaching
+/*@MapperScan(basePackages="com.lcclovehww.springboot.chapter12", annotationClass = Mapper.class)
+@EnableCaching*/
 public class Chapter12Application extends WebSecurityConfigurerAdapter{
 	
 /*	@Value("${system.user.password.secret}")
 	private String secret = null;*/
 	
-	@Autowired
-	UserDetailsService userDetailsService= null;
+/*	@Autowired
+	UserDetailsService userDetailsService= null;*/
 
 /*	@Autowired
 	private DataSource dataSource = null;*/
 	
-	String pwdQuery = "select user_name, pwd, available " + "from t_user where user_name=?";
+/*	String pwdQuery = "select user_name, pwd, available " + "from t_user where user_name=?";
 	
 	String roleQuery = "select u.user_name, r.role_name"
 						+" from t_user u, t_user_role ur, t_role r"
 						+" where u.id = ur.user_id and r.id=ur.role_id"
-						+" and u.user_name = ?";
+						+" and u.user_name = ?";*/
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter12Application.class, args);
@@ -99,15 +85,15 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 		.authoritiesByUsernameQuery(roleQuery);
 	}*/
 	
-	@Override
+/*	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
 		
-	}
+	}*/
 	
-	@Override
+/*	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception{
 		httpSecurity.authorizeRequests().antMatchers("/admin/**").access("hasRole('ADMIN')")
 		.and().rememberMe().tokenValiditySeconds(86400).key("remember-me-key")
@@ -115,6 +101,6 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 		.and().authorizeRequests().antMatchers("/**").permitAll()
 		.and().formLogin().loginPage("/login/page")
 		.defaultSuccessUrl("/admin/welcome1");
-	}
+	}*/
 }
 
