34f65b2ed29b7082353236efb72431e1faea631e
bug123luo <luo_3050120123@163.com>
2019-02-13 11:05:38 +0800

    12-7实例 使用数据库验证

--- springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
+++ springboot2x/Chapter12/src/main/java/com/lcclovehww/springboot/chapter12/main/Chapter12Application.java
@@ -1,9 +1,12 @@
 package com.lcclovehww.springboot.chapter12.main;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+//import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,15 +22,15 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 /*	@Autowired
 	UserDetailsService userDetailsService= null;*/
 
-/*	@Autowired
-	private DataSource dataSource = null;*/
+	@Autowired
+	private DataSource dataSource = null;
 	
-/*	String pwdQuery = "select user_name, pwd, available " + "from t_user where user_name=?";
+	String pwdQuery = "select user_name, pwd, available " + "from t_user where user_name=?";
 	
 	String roleQuery = "select u.user_name, r.role_name"
 						+" from t_user u, t_user_role ur, t_role r"
 						+" where u.id = ur.user_id and r.id=ur.role_id"
-						+" and u.user_name = ?";*/
+						+" and u.user_name = ?";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Chapter12Application.class, args);
@@ -60,7 +63,7 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 	}*/
 	
 	//和上一个函数实现的方法是相同的
-	@Override
+/*	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder> userConfig = auth.inMemoryAuthentication().passwordEncoder(passwordEncoder);
@@ -70,10 +73,10 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 		userConfig.withUser("myuser")
 				  .password(passwordEncoder.encode("123456"))
 				  .authorities("ROLE_USER");
-	}
+	}*/
 	
 	//使用数据库定义的用户认证服务
-/*	@Override
+	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
 		PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		
@@ -83,7 +86,7 @@ public class Chapter12Application extends WebSecurityConfigurerAdapter{
 			.usersByUsernameQuery(pwdQuery)
 			.authoritiesByUsernameQuery(roleQuery);
 		
-	}*/
+	}
 	
 /*	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception{
